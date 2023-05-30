6b84bf6a5893c13fc9b3a2c013d75d3bda445203
Gerson Janhuel <gersonjanhuel@gmail.com>
2023-05-30 23:52:09 +0700

    Initial Commit

---
+++ Starter_PropertyWrappersPart2_HandsOn/Starter_PropertyWrappersPart2_HandsOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  Starter_PropertyWrappersPart2_HandsOnApp.swift
+//  Starter_PropertyWrappersPart2_HandsOn
+//
+//  Created by Gerson Janhuel on 30/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Starter_PropertyWrappersPart2_HandsOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
